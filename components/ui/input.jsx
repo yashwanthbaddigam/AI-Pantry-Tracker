0a8db5df4f69f753b8c795bd5bd4ec8cafa14ddb
yashwanthbaddigam <134735579+yashwanthbaddigam@users.noreply.github.com>
2025-03-26 20:43:21 +0530

    Add files via upload

---
+++ components/ui/input.jsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+  return (
+    (<input
+      type={type}
+      className={cn(
+        "file:bg-inherit file:rounded-md file:border-collapse file:text-cyan-100 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props} />)
+  );
+})
+Input.displayName = "Input"
+
+export { Input }
